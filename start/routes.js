440966a2f5ad441b4d0ed139a2167121e7b19bbf
candra handrian <handrian.ch@gmal.com>
2019-07-08 21:49:51 +0700

    uncomment middleware

--- start/routes.js
+++ start/routes.js
@@ -26,4 +26,4 @@ Route.group(() => {
   Route.get('/crosswords', 'CrosswordController.index')
   Route.get('/crosswords/:id/answers', 'CrosswordController.answers')
   Route.get('/crosswords/:id/user-answers', 'CrosswordController.userAnswers')
-}).prefix('api/v1') // .middleware('auth')
+}).prefix('api/v1').middleware('auth')
